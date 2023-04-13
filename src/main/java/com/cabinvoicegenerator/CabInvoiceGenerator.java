2872d3be082ff566497d38a0ecb06b8f1a5c8690
akhilmondal <akhilmandalfamily@gmail.com>
2023-04-13 21:49:45 +0530

    Added: Program added to show welcome message to the master.

---
+++ src/main/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -0,0 +1,9 @@
+package com.cabinvoicegenerator;
+
+public class CabInvoiceGenerator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Cab Invoice Generator: ");
+    }
+}
---
+++ src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,4 @@
+package com.cabinvoicegenerator;
+
+public class CabInvoiceGeneratorTest {
+}
