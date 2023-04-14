d2eddd265bfed1476a9e355054e9ab4fd053d60c
akhilmondal <akhilmandalfamily@gmail.com>
2023-04-14 18:47:49 +0530

    Added: Program to calculate Fare.

--- src/main/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,13 +1,21 @@
 package com.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
+    static final double COST_PER_KM = 10;
+    static final double COST_PER_MIN = 1;
+
+    public static double calculateFare(double distance, int time) {
+
+        double fare = COST_PER_KM * distance + COST_PER_MIN * time;
+        return fare < 5 ? 5 : fare;
+    }
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Cab Invoice Generator: ");
+        calculateFare(20,30);
     }
 
-    public double calculateFare(double distance, int time) {
 
-    }
+
 }
--- src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -1,14 +1,11 @@
 package com.cabinvoicegenerator;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class CabInvoiceGeneratorTest {
-    CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+    static CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
 
-    @Test
-    double distance = 2.0;
-    int time = 5;
-    double result = cabInvoiceGenerator.calculateFare(distance, time);
-    Assertions.assertEquals(25, result);
+
 }
