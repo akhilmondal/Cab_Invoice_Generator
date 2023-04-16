3f35b6a66276cd80ca768f91eb33a811953caf0e
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-04-16 21:49:16 +0530

    Added: Program of the Multiple Rides

--- src/main/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -5,18 +5,28 @@ public class CabInvoiceGenerator {
     static final double COST_PER_KM = 10;
     static final double COST_PER_MIN = 1;
 
-    public static double calculateFare(double distance, int time) {
+    public static double calculateFare(double distance, double time) {
 
         double fare = COST_PER_KM * distance + COST_PER_MIN * time;
         return fare < 5 ? 5 : fare;
     }
+    public double calculateFare(Ride[] rides) {
+        double aggregateFare = 0 ;
+        for (Ride ride:rides) {
+            double totalFare = calculateFare(ride.getDistance(), ride.getTime());
+            aggregateFare += totalFare;
 
-    public static void main(String[] args) {
+        }
+        return aggregateFare;
+    }
+        public static void main(String[] args) {
 
         System.out.println("Welcome to Cab Invoice Generator: ");
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         calculateFare(20,30);
+        Ride[] rides = {new Ride(0.4, 0.5), new Ride(5.5, 3.5), new Ride(2.5, 2.3)};
+
+        double totalFare =  cabInvoiceGenerator.calculateFare(rides);
+        System.out.println(totalFare);
     }
-
-
-
 }
--- src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -9,7 +9,7 @@ public class CabInvoiceGeneratorTest {
     @Test
     public void testCabInvoiceGenerator() { //step1: This  is my test case for Calculate Fare
         double distance = 2.0;
-        int time = 5;
+        double time = 5.0;
         double result = cabInvoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, result,0.0);
     }
